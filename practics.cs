8c5089624076186a6026b3296a3d322b207e445c
utharr <morntide@hotmail.com>
2026-08-17 12:53:14 +0800

    first time to pull a Cshacrp file

---
+++ practics.cs
@@ -0,0 +1,11 @@
+namespace plactics_01
+{
+public class say_hello
+{
+    public static void Dosomething(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+}
---
+++ programs.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using  plactics_01;
+
+say_hello.Dosomething("hello world! beautiful world!");
